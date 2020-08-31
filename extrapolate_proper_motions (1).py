2e4bcdb5f69dcb901f8754d32976600d8eda270f
Emma Lieb <41973742+emmalieb@users.noreply.github.com>
2020-08-31 08:00:50 -0600

    Add files via upload

---
+++ extrapolate_proper_motions (1).py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#get proper motions of GAIA stars from search and run them backwards to find where the stars were X Myrs ago
+
---
+++ plotting.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#general file for all plotting needs (keeps the main program neat)
+
