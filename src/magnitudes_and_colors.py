f2d8cbb1fa8de73d4cdf61838deac2ebfb47b40f
Emma Lieb <Emma.Lieb@lasp.colorado.edu>
2020-09-20 21:23:20 -0600

    Update magnitudes_and_colors for candidate_members
    Needs work! Also, deleted calculate_cluster_values, as gaia stars already does this for us.

--- src/magnitudes_and_colors.py
+++ src/magnitudes_and_colors.py
@@ -13,8 +13,9 @@ import os
 #need to calculate color index of stars in cluster based on their magnitudes in certain filters
 #this function will get the known members absolute magnitudes of our known cluster members
 def getMagsKnownMembers(known_members):
-    #get known members
-    #known_members = getGaiaKnownMembers(cluster_name) <--I realized I will pass this in 
+    '''Returns the color excess of the known_members
+     using the distance to the sun the stars are and their gaia filter magnitudes
+     ''' 
     #get gaia data for the known members
     known_gaia = gs(cluster_name+': Known Members')
     known_gaia.from_source_idlist(known_members.index.to_list())
@@ -34,6 +35,9 @@ def getMagsKnownMembers(known_members):
 
 #this function will get the color index of known members
 def getColorExcessKnownMembers(known_members):
+    '''Returns the color excess of the known_members
+     using the distance to the sun the stars are and their gaia filter magnitudes
+     '''
     #get gaia data for the known members
     known_gaia = gs(cluster_name+': Known Members')
     known_gaia.from_source_idlist(known_members.index.to_list())
@@ -43,23 +47,33 @@ def getColorExcessKnownMembers(known_members):
     #do we want to do a different color index? this is blue minus red I think
     return(bp_rp)
 
-#this function will convert filter types of gaia magnitudes to different ones because I think this is necessary?
+
+# #this function will get the color index of candidate members, necessary?
+def getColorExcessCandidates(candidate_members):
+    '''Returns the color excess of the candidate_members
+     using the distance to the sun the stars are and their gaia filter magnitudes
+    To update: specify how candidate_members are passed in and how it works with gaiastars format
+    '''
+    dist_mod = 5*np.log10(candidate_members.obj.d)-5
+    bp_rp = candidate_members.objs.phot_bp_mean_mag - candidate_members.objs.phot_rp_mean_mag
+    return(bp_rp)
+    
+
+def getAppMagsCandidateMembers(candidate_members):
+    '''Returns the apparent magnitudes of the candidate members 
+    using the distance and the magnitude of the members in the gaia red filter.
+    To update: specify how candidate_members are passed in and how it works with gaiastars format
+    '''
+    #based on distance, get apparent mag of candidates
+    distanceFromSun = candidate_members.obj.ObjDistCen
+    #magnitude in gaia red filter? 
+    rp = known_gaia.objs.phot_rp_mean_mag 
+    #I am not sure if this is right 
+    m = 5*np.log10(distance/10)+rp
+    return(m)
+
+#this function will convert filter types of gaia magnitudes to different ones, this may not be necessary.
 # def convertFilters(mag_to_convert, new_filter):
     #gaia is UGRIZ I think? 
     #Cousins-Johnson?
     #what other fitler types should we include?
-
-
-"""
-    These next 2 functions will have to pass in the candidate members and 
-    I need the stars after the proper motion trace back to do this, 
-    also will the stars passed in be compatible with gaia_stars?
-"""
-# #this function will get the color index of candidate members 
-# def getColorExcessCandidates(candidate_members):
-#     
-# #this function will get magnitudes of our candidate members 
-# def getMagsCandidateMembers(candidate_members):
-#get distance to candidates from sun
-#based on distance, get apparent mag of candidates
-#assuming they are on the main sequence 
